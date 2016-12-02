37c2c8b4ee2f305fa24814a9862c4a4350706a98
Ryan Rogalski <ryan.rogalski@gmail.com>
2016-12-01 19:34:59 -0800

    add glider

--- src/game.js
+++ src/game.js
@@ -1,7 +1,46 @@
+const glider = [
+  [1, 5],
+  [1, 6],
+  [2, 5],
+  [2, 6],
+  [11, 5],
+  [11, 6],
+  [11, 7],
+  [12, 4],
+  [12, 8],
+  [13, 3],
+  [13, 9],
+  [14, 3],
+  [14, 9],
+  [15, 6],
+  [16, 4],
+  [16, 8],
+  [17, 5],
+  [17, 6],
+  [17, 7],
+  [18, 6],
+  [21, 3],
+  [21, 4],
+  [21, 5],
+  [22, 3],
+  [22, 4],
+  [22, 5],
+  [23, 2],
+  [23, 6],
+  [25, 1],
+  [25, 2],
+  [25, 6],
+  [25, 7],
+  [35, 3],
+  [35, 4],
+  [36, 3],
+  [36, 4]
+]
+
 class GameOfLife {
   constructor(){
     this.board = document.querySelector('.board')
-    this.fps = 1000
+    this.fps = 10
     this.interval = 1000 / this.fps
     this.animation = null 
     this.then = 0
@@ -29,22 +68,12 @@ class GameOfLife {
 
   configBoard() {
     const arr = this.getCells()
-    const r = 10 + Math.floor(Math.random()*80)
-
-    const cells = [
-      arr[r-1][r-1],
-      arr[r-1][r],
-      arr[r-1][r+1],
-
-      arr[r][r-1],
-      arr[r][r+1],
-
-      arr[r+1][r-1],
-      arr[r+1][r],
-      arr[r+1][r+1],
-    ]
-
-    cells.forEach(i => i.classList.add('live'))
+    
+    glider.forEach(i => {
+      const x = i[1]
+      const y = i[0]
+      arr[x][y].classList.add('live')
+    })
 
     this.setupEvents()
   }
@@ -60,6 +89,7 @@ class GameOfLife {
         this.startGame()
       }
     })
+    this.startGame()
   }
 
   getCells() {
