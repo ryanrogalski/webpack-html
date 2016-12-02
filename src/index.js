9a35eca370020e066e8c0abdfeeb83c6f0dc5a77
Ryan Rogalski <ryan.rogalski@gmail.com>
2016-12-01 23:05:02 -0800

    reverse list html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 require('./index.html')
+require('./style.css')
+
+let list = [...document.querySelectorAll('li')].reverse()
+
+console.log(list);
+
+const ul = document.querySelector('ul')
+
+ul.innerHTML = ''
+
+list.forEach(i => {
+  const li = document.createElement('li')
+  li.textContent = i.textContent
+  ul.appendChild(li)
+})
