a7f5b8a9d6aceb69300136e777834c1737198e9a
Ryan Rogalski <ryan.rogalski@gmail.com>
2016-12-01 20:17:08 -0800

    progress

--- src/game.js
+++ src/game.js
@@ -1,7 +1,3 @@
-// const glider = [
-//   [0, 0],
-//   [10, 0]
-// ]
 const glider = [
   [1, 5],
   [1, 6],
@@ -46,37 +42,10 @@ class Life {
     this.canvas = document.querySelector('canvas')
     this.ctx = this.canvas.getContext('2d')
     this.len = 64
+    this.board = []
     this.sizeCanvas()
     this.createGrid()
     this.addEvents()
-  }
-
-  addEvents() {
-    window.addEventListener('resize', () => this.sizeCanvas())
-  }
-
-  createGrid() {
-    const cells = []
-
-    for (let i=0;i<this.len;i++){
-      cells[i] = []
-      for (let j=0;j<this.len;j++){
-        cells[i][j] = 0       
-      }
-    }
-
-    this.cells = cells
-    this.addGlider()
-  }
-
-  addGlider() {
-    glider.forEach(i => {
-      const x = i[0]
-      const y = i[1]
-      this.cells[x][y] = 1
-    })
-
-    this.draw(this.cells)
   }
 
   sizeCanvas() {
@@ -85,18 +54,58 @@ class Life {
     this.size = this.canvas.width / this.len
   }
 
+  addEvents() {
+    window.addEventListener('resize', () => this.sizeCanvas())
+  }
+
+  createGrid() {
+    const board = []
+
+    for (let i = 0; i < this.len; i++) {
+      board[i] = []
+      for (let j = 0; j < this.len; j++) {
+        board[i][j] = 0
+      }
+    }
+
+    // this.addGrid(board)
+    this.addGlider(board)
+  }
+
+  addGrid(board) {
+    board.forEach((row, x) => {
+      row.forEach((cell, y) => {
+        if (x % 2 && y % 2) board[x][y] = 1
+      })
+    })
+
+    this.board = board
+    this.draw()
+  }
+
+  addGlider(board) {
+    glider.forEach(i => {
+      const x = i[0]
+      const y = i[1]
+      board[x][y] = 1
+    })
+    this.board = board
+    this.draw()
+  }
+
   draw() {
-    const ctx = this.ctx 
+    const ctx = this.ctx
     const size = this.size
+    const board = this.board
 
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
     ctx.strokeStyle = '#e1e1e1'
     ctx.fillStyle = '#67DDDD'
-    
-    for (const [x, row] of this.cells.entries()){
-      for (const [y, cell] of row.entries()){
+
+    for (const [x, row] of board.entries()) {
+      for (const [y, cell] of row.entries()) {
         ctx.beginPath()
-        ctx.rect(x*size, y*size, size, size)
+        ctx.rect(x * size, y * size, size, size)
 
         if (cell === 0) {
           ctx.stroke()
@@ -106,56 +115,59 @@ class Life {
       }
     }
 
-    requestAnimationFrame(() => this.updateCells())
-    // setTimeout(() => this.updateCells(), 1500)
+    // requestAnimationFrame(() => this.updateCells())
+    setTimeout(() => this.updateCells(), 500)
   }
 
   updateCells() {
-    console.log('update');
     const arr = []
+    const board = this.board
 
-    for (const [x, row] of this.cells.entries()){
+    board.forEach((row, x) => {
       arr[x] = []
-      for (const [y, cell] of row.entries()){
-        
+      row.forEach((cell, y) => {
         let val = 0
-        const neighbors = this.countNeighbors(x, y)
+        const neighbors = this.countNeighbors(x, y, board)
 
         // live cell
-        if (cell > 0) {
+        if (cell === 1) {
           val = (neighbors === 2 || neighbors === 3) ? 1 : 0
+            // dead cell
         } else {
           val = (neighbors === 3) ? 1 : 0
         }
 
         arr[x][y] = val
-      }
-    }    
+      })
+    })
+
+    this.board = arr
 
     this.draw()
   }
 
-  countNeighbors(x, y) {
+  countNeighbors(x, y, board) {
     let amount = 0
+    const end = this.len - 1
 
-    const isFilled = (x, y) => {
-      return this.cells[x] && this.cells[x][y]      
+    function checkCell(x, y) {
+      if (x < 0 || y < 0 || x > end || y > end) return false
+      if (board[x][y] === 1) return true
     }
 
-    if (isFilled(x - 1, y - 1)) amount++
-    if (isFilled(x - 1, y)) amount++
-    if (isFilled(x - 1, y + 1)) amount++
+    if (checkCell(x - 1, y - 1)) amount++
+    if (checkCell(x - 1, y    )) amount++
+    if (checkCell(x - 1, y + 1)) amount++
 
-    if (isFilled(x - 1, y)) amount++
-    if (isFilled(x + 1, y)) amount++
+    if (checkCell(x - 1, y    )) amount++
+    if (checkCell(x + 1, y    )) amount++
 
-    if (isFilled(x + 1, y - 1)) amount++
-    if (isFilled(x + 1, y)) amount++
-    if (isFilled(x + 1, y + 1)) amount++
+    if (checkCell(x + 1, y - 1)) amount++
+    if (checkCell(x + 1, y    )) amount++
+    if (checkCell(x + 1, y + 1)) amount++
 
     return amount
   }
-
 }
 
 new Life()
