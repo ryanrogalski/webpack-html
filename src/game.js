07ae2b7e272f0e53845fa8dff0974cfc56e28d91
Ryan Rogalski <ryan.rogalski@gmail.com>
2016-12-01 21:17:09 -0800

    class and RAF

--- src/game.js
+++ src/game.js
@@ -1,173 +1,118 @@
-const glider = [
-  [1, 5],
-  [1, 6],
-  [2, 5],
-  [2, 6],
-  [11, 5],
-  [11, 6],
-  [11, 7],
-  [12, 4],
-  [12, 8],
-  [13, 3],
-  [13, 9],
-  [14, 3],
-  [14, 9],
-  [15, 6],
-  [16, 4],
-  [16, 8],
-  [17, 5],
-  [17, 6],
-  [17, 7],
-  [18, 6],
-  [21, 3],
-  [21, 4],
-  [21, 5],
-  [22, 3],
-  [22, 4],
-  [22, 5],
-  [23, 2],
-  [23, 6],
-  [25, 1],
-  [25, 2],
-  [25, 6],
-  [25, 7],
-  [35, 3],
-  [35, 4],
-  [36, 3],
-  [36, 4]
-]
+import grid from './grid'
 
-class Life {
+class GameOfLife {
   constructor() {
     this.canvas = document.querySelector('canvas')
     this.ctx = this.canvas.getContext('2d')
-    this.len = 64
-    this.board = []
-    this.sizeCanvas()
-    this.createGrid()
-    this.addEvents()
-  }
+    this.cells = []
+    this.side = 100
 
-  sizeCanvas() {
-    this.canvas.width = window.innerWidth / 2
+    this.fps = 30
+    this.interval = 1000 / this.fps
+    this.end = 0
+
+    this.setupCanvas()
+    this.buildGrid()
+  }
+  
+  setupCanvas() {
+    this.canvas.width = window.innerWidth
     this.canvas.height = this.canvas.width
-    this.size = this.canvas.width / this.len
+    this.width = this.canvas.width
+    this.cellSize = this.canvas.width / this.side;
+
+    window.addEventListener('resize', () => {
+      if (this.width !== window.innerWidth) this.setupCanvas()
+    })
   }
 
-  addEvents() {
-    window.addEventListener('resize', () => this.sizeCanvas())
-  }
-
-  createGrid() {
-    const board = []
-
-    for (let i = 0; i < this.len; i++) {
-      board[i] = []
-      for (let j = 0; j < this.len; j++) {
-        board[i][j] = 0
+  buildGrid() {
+    for (var i = 0; i < this.side; i++) {
+      this.cells[i] = []
+      for (var j = 0; j < this.side; j++) {
+        this.cells[i][j] = 0
       }
     }
 
-    // this.addGrid(board)
-    this.addGlider(board)
-  }
-
-  addGrid(board) {
-    board.forEach((row, x) => {
-      row.forEach((cell, y) => {
-        if (x % 2 && y % 2) board[x][y] = 1
-      })
-    })
-
-    this.board = board
-    this.draw()
-  }
-
-  addGlider(board) {
-    glider.forEach(i => {
+    grid.forEach(i => {
       const x = i[0]
       const y = i[1]
-      board[x][y] = 1
+      this.cells[x][y] = 1
     })
-    this.board = board
+
     this.draw()
   }
 
-  draw() {
-    const ctx = this.ctx
-    const size = this.size
-    const board = this.board
+  update(ts) {
+    let elapsed = ts - this.end
 
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.strokeStyle = '#e1e1e1'
-    ctx.fillStyle = '#67DDDD'
+    var arr = []
 
-    for (const [x, row] of board.entries()) {
-      for (const [y, cell] of row.entries()) {
-        ctx.beginPath()
-        ctx.rect(x * size, y * size, size, size)
+    const countNeighbours = (x, y) => {
+      var count = 0
 
-        if (cell === 0) {
-          ctx.stroke()
-        } else {
-          ctx.fill()
-        }
-      }
+      const checkCell = (x, y) => this.cells[x] && this.cells[x][y]
+
+      if (checkCell(x - 1, y - 1)) count++
+      if (checkCell(x, y - 1)) count++
+      if (checkCell(x + 1, y - 1)) count++
+      if (checkCell(x - 1, y)) count++
+      if (checkCell(x + 1, y)) count++
+      if (checkCell(x - 1, y + 1)) count++
+      if (checkCell(x, y + 1)) count++
+      if (checkCell(x + 1, y + 1)) count++
+
+      return count
     }
 
-    // requestAnimationFrame(() => this.updateCells())
-    setTimeout(() => this.updateCells(), 500)
+    if (elapsed > this.interval) {
+      this.end = ts - (elapsed % this.interval)
+
+      this.cells.forEach(function(row, x) {
+        arr[x] = []
+        row.forEach(function(cell, y) {
+          var alive = 0,
+            count = countNeighbours(x, y)
+
+          if (cell > 0) {
+            alive = count === 2 || count === 3 ? 1 : 0
+          } else {
+            alive = count === 3 ? 1 : 0
+          }
+
+          arr[x][y] = alive
+        })
+      })
+
+      this.cells = arr
+    }
+
+    this.draw(ts)
   }
 
-  updateCells() {
-    const arr = []
-    const board = this.board
+  draw(ts) {
+    const ctx = this.ctx
+    const size = this.cellSize
 
-    board.forEach((row, x) => {
-      arr[x] = []
-      row.forEach((cell, y) => {
-        let val = 0
-        const neighbors = this.countNeighbors(x, y, board)
+    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    ctx.strokeStyle = '#E8E8E8'
+    ctx.lineWidth = .5
+    ctx.fillStyle = '#658FFF'
 
-        // live cell
-        if (cell === 1) {
-          val = (neighbors === 2 || neighbors === 3) ? 1 : 0
-            // dead cell
+    this.cells.forEach(function(row, x) {
+      row.forEach(function(cell, y) {
+        ctx.beginPath()
+        ctx.rect(x * size, y * size, size, size)
+        if (cell) {
+          ctx.fill()
         } else {
-          val = (neighbors === 3) ? 1 : 0
+          ctx.stroke()
         }
-
-        arr[x][y] = val
       })
     })
 
-    this.board = arr
-
-    this.draw()
-  }
-
-  countNeighbors(x, y, board) {
-    let amount = 0
-    const end = this.len - 1
-
-    function checkCell(x, y) {
-      if (x < 0 || y < 0 || x > end || y > end) return false
-      if (board[x][y] === 1) return true
-    }
-
-    if (checkCell(x - 1, y - 1)) amount++
-    if (checkCell(x - 1, y    )) amount++
-    if (checkCell(x - 1, y + 1)) amount++
-
-    if (checkCell(x - 1, y    )) amount++
-    if (checkCell(x + 1, y    )) amount++
-
-    if (checkCell(x + 1, y - 1)) amount++
-    if (checkCell(x + 1, y    )) amount++
-    if (checkCell(x + 1, y + 1)) amount++
-
-    return amount
+    this.animate = requestAnimationFrame((ts) => this.update(ts))
   }
 }
 
-new Life()
+new GameOfLife()
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -22,6 +22,13 @@ module.exports = {
   ],
   module: {
     loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }, {
       test: /\.css$/,
       loaders: ['style', 'css']
     }, {
