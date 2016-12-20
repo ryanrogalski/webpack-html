f06a0e7f1b2e55aa8cdeb137c7fe9c583268ef58
Ryan Rogalski <ryan.rogalski@gmail.com>
2016-12-20 00:20:04 -0800

    app solutions

--- src/code.js
+++ src/code.js
@@ -1,12 +1,28 @@
-function createButtons(){
-  const arr = Array(10).fill(0).map((e, i) => e = i+1)
+const btns = [...document.querySelectorAll('.btn')]
 
-  arr.forEach(i => {
-    const btn = document.createElement('button')
-    btn.textContent = `Button ${i}`
-    btn.addEventListener('click', () => alert(i))
-    document.body.appendChild(btn)
+btns.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    alert(e.target.textContent)
   })
-}
+})
 
-createButtons()
+fetch('https://app.close.io/hackwithus/', {
+  method: 'POST',
+  mode: 'no-cors',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    first_name: 'Ryan',
+    last_name: 'Rogalski',
+    email: 'ryan@infiniteloopco.com',
+    phone: '518.810.5365',
+    cover_letter: 'Hi,\nI’m Ryan, a product focused front end engineer who specializes in building modern web applications with React, ES6, and Sass. For the last 4 years I’ve been consulting with clients around the world through my creative studio Infinite Loop\nCheck out my work on my portfolio at www.infiniteloopco.com (which I recently rebuilt in React), browse my experiments on Codepen (www.codepen.io/ryanrogalski), or read my ramblings on Twitter (www.twitter.com/ryanrogalski).\nI have two personal projects I think you’ll enjoy checking out. One is Gifstudio, which lets you use your laptop camera to create and share experimental animated GIFs. Built with React and WebGL video processing.\nwww.gifstudio.me\nThe other is Gifgasm, a psychedelic search engine for Tumblr gifs.\nwww.gifgasm.me/@ryanrogalski\nThanks for considering me for the job, if you’d like to talk more feel free to get in touch.\nHappy Holidays,\nRyan',
+    urls: [
+      'www.infiniteloopco.com',
+      'www.ryanrogalski.com',
+      'www.gifgasm.me',
+      'www.gifstudio.me'
+    ],
+  })
+})
