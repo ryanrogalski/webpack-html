1a6667532b87b686bbbd97bd270866460681f302
Ryan Rogalski <ryan@infiniteloopco.com>
2020-03-26 11:04:12 -0700

    add prettier

--- src/code.js
+++ src/code.js
@@ -1,4 +1,21 @@
-const btn = document.createElement('button')
-btn.innerHTML = "Howdy"
+function removeDuplicates(nums) {
+  if (nums.length === 0) return 0
 
-document.querySelector('body').append(btn)
+  let i = 0
+
+  for (j = 0; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      console.log(nums[j], nums[i], i, j)
+
+      i++
+      nums[i] = nums[j]
+    }
+  }
+
+  // return i + 1
+  return nums
+}
+
+console.log(
+  removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]),
+)
