dbd2ebf81c136545f50fcf15d6679475771f81de
Ryan Rogalski <ryan@infiniteloopco.com>
2020-03-26 15:37:19 -0700

    sudoku

--- src/code.js
+++ src/code.js
@@ -1,21 +1,69 @@
-function removeDuplicates(nums) {
-  if (nums.length === 0) return 0
+const invalidBoard = [
+  [5, 3, 4, 6, 7, 9, 8, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 7],
+  [6, 9, 8, 3, 4, 2, 7, 6, 5],
 
-  let i = 0
+  [8, 5, 9, 7, 6, 1, 4, 2, 1],
+  [4, 2, 6, 8, 5, 3, 7, 8, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
 
-  for (j = 0; j < nums.length; j++) {
-    if (nums[j] !== nums[i]) {
-      console.log(nums[j], nums[i], i, j)
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 8, 8],
+]
 
-      i++
-      nums[i] = nums[j]
-    }
-  }
+const validBoard = [
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
 
-  // return i + 1
-  return nums
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9],
+]
+
+function checkArray(arr) {
+  const set = new Set(arr)
+  return set.size === arr.length
+}
+
+function getCells(board) {
+  const cells = [[], [], [], [], [], [], [], [], []]
+
+  board.forEach((row, rowIndex) => {
+    row.forEach((col, colIndex) => {
+      const cellIndex =
+        Math.floor(rowIndex / 3) * 3 +
+        Math.floor(colIndex / 3)
+
+      cells[cellIndex].push(col)
+    })
+  })
+
+  return cells
+}
+
+function getRows(board) {
+  return board.map((row, i) => board.map((item) => item[i]))
+}
+
+function validateBoard(board) {
+  const xValid = board.every(checkArray)
+
+  const yValid = getRows(board).every(checkArray)
+
+  const cellsValid = getCells(board).every(checkArray)
+
+  return xValid && yValid && cellsValid
 }
 
 console.log(
-  removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]),
+  'invalid board:',
+  validateBoard(invalidBoard),
+  '\nvalidboard:',
+  validateBoard(validBoard),
 )
