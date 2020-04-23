f2a41155101efe42a860e8c78817c4fd834a1224
Ryan Rogalski <ryan@infiniteloopco.com>
2020-04-23 14:21:22 -0700

    better demo

--- src/code.js
+++ src/code.js
@@ -1,2 +1,11 @@
-// put your codez here
-document.body.append('howdy!')
+// javascript you add here will be imported to index.js and bundled by webpack
+const wrap = document.createElement('div')
+wrap.classList.add('wrapper')
+
+const el = document.createElement('div')
+el.classList.add('welcome')
+el.innerText = 'Welcome!'
+
+wrap.append(el)
+
+document.body.append(wrap)
